4d871f2f3d0f7b0db9ebd19a1feb090253129797
Claire Saunders <clamwoodsaunders@gmail.com>
2026-01-17 13:54:42 -0800

    pyproject.toml

--- tests/unit/test_check_multicollinearity.py
+++ tests/unit/test_check_multicollinearity.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from lrassume.check_multicollinearity import check_multicollinearity_vif
+from lrassume import check_multicollinearity_vif
 
 @pytest.fixture
 def df_numeric_ok():
